25b87ceaf7351bd65fe874510386e548b2da230e
P41nwt55 <ederoliveira15@gmail.com>
2020-01-07 13:35:18 -0500

    Upload do site

---
+++ script/script.js
@@ -0,0 +1,13 @@
+var tituloPrincipal;
+ 
+ function tituloInteiro(){
+    tituloPrincipal = document.querySelector('.tituloPrincipal');
+     tituloPrincipal.textContent = 'Eder Developer';
+     
+ }
+ function tituloResumido(){
+    tituloPrincipal = document.querySelector('.tituloPrincipal');
+     tituloPrincipal.textContent = 'EDev';
+     
+ }
+ 
